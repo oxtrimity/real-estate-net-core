96e61f2db8b295fdd79ecce2301c7875ebe5037b
oxtrimity <oxtrimity@gmail.com>
2022-02-05 20:54:25 +0800

    Added core & test projects.

---
+++ RealEstateApp.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RealEstateApp.FunctionalTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
